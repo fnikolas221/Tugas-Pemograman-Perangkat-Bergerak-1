93d3d6bd86e6412ea8c69da7c5ae062fe4582185
Nikofn20201 <nikolasfernando2001@gmail.com>
2023-03-10 00:28:03 +0800

    Refactor Code

--- lib/login.dart
+++ lib/login.dart
@@ -19,58 +19,60 @@ class _LoginState extends State<Login> {
         body: Container(
           padding: EdgeInsets.symmetric(vertical:200),
           child: Center(
-            child: Column(
-              children: [
-                Text("Login Aplikasi Data Diri",style: TextStyle(color: Colors.black87, fontSize: 30)),
-                SizedBox(height: 20),
-                Container(
-                  padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                  child: TextField(
-                    onChanged: (text){
-                      setState(() {
-                        usernameText = text;
-                      });
-                    },
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(90.0),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Text("Login Aplikasi Data Diri",style: TextStyle(color: Colors.black87, fontSize: 30)),
+                  SizedBox(height: 20),
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                    child: TextField(
+                      onChanged: (text){
+                        setState(() {
+                          usernameText = text;
+                        });
+                      },
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(90.0),
+                        ),
+                        labelText: 'Username',
                       ),
-                      labelText: 'Username',
                     ),
                   ),
-                ),
-                SizedBox(height: 20),
-                Container(
-                  padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                  child: TextField(
-                    onChanged: (text){
-                      setState(() {
-                        passwordText = text;
-                      });
-                    },
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(90.0),
+                  SizedBox(height: 20),
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                    child: TextField(
+                      onChanged: (text){
+                        setState(() {
+                          passwordText = text;
+                        });
+                      },
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(90.0),
+                        ),
+                        labelText: 'Password',
                       ),
-                      labelText: 'Password',
                     ),
                   ),
-                ),
-                SizedBox(height: 20),
-                Container(
-                  padding: const EdgeInsets.fromLTRB(25,3 , 25, 3),
-                  color: Colors.blue,
-                  child: TextButton(onPressed: (){
-                    if(usernameText=="android" && passwordText=="android"){
-                      Navigator.pushReplacementNamed(context, "/home",arguments: {"username":usernameText});
-                    }else{
-                      print("Username dan password salah");
-                    }
-                  },child: Text("Login",style: TextStyle(color: Colors.white),))
-                ),
-              ],
+                  SizedBox(height: 20),
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(25,3 , 25, 3),
+                    color: Colors.blue,
+                    child: TextButton(onPressed: (){
+                      if(usernameText=="android" && passwordText=="android"){
+                        Navigator.pushReplacementNamed(context, "/home",arguments: {"username":usernameText});
+                      }else{
+                        print("Username dan password salah");
+                      }
+                    },child: Text("Login",style: TextStyle(color: Colors.white),))
+                  ),
+                ],
+              ),
             ),
           ),
         ),
